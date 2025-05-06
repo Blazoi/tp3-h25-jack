c4075f9c086e21747718a010d6e6516c9777242c
Jack Adam's Dieujuste <2365768@bdeb.qc.ca>
2025-05-06 09:59:45 -0400

    Implementation de la page de vente

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
 const root = document.querySelector(":root");
 const cs = getComputedStyle(root);
 const btnmode = document.querySelector(".boutonmode");
+const btncompte = document.querySelector(".boutoncompte");
 
 let clair = localStorage.getItem("mode") === "false";
 
+// Bouton mode sombre hover
 btnmode.addEventListener("mouseenter", function () {
   btnmode.style.color = cs.getPropertyValue("--couleur4");
 });
 btnmode.addEventListener("mouseleave", function () {
   btnmode.style.color = cs.getPropertyValue("--couleur3");
 });
+// Bouton compte hover
+btncompte.addEventListener("mouseenter", function () {
+  btncompte.style.color = cs.getPropertyValue("--couleur4");
+});
+btncompte.addEventListener("mouseleave", function () {
+  btncompte.style.color = cs.getPropertyValue("--couleur3");
+});
+
+// Bouton panier hover
+
+const btnpanier = document.querySelector(".boutonpanier");
+const btnpaniericn = document.querySelector(".bx-cart-alt");
+btnpanier.addEventListener("mouseenter", function () {
+  btnpanier.innerHTML = 'Panier <i class="bx bxs-cart-alt"></i>';
+});
+btnpanier.addEventListener("mouseleave", function () {
+  btnpanier.innerHTML = 'Panier <i class="bx bx-cart-alt"></i>';
+});
+
 
 if (localStorage.getItem("sombreclair") == "clair") {
   modeclair();
@@ -36,12 +57,56 @@ function modesombre() {
   localStorage.setItem("sombreclair", "sombre");
 }
 function modeclair() {
-  root.style.setProperty("--couleur1", "#f5f5f5");
+  root.style.setProperty("--couleur1", "#e0e0e0");
   root.style.setProperty("--couleur2", "#f8f8f8");
   root.style.setProperty("--couleur3", "#000000");
-  root.style.setProperty("--couleur4", "#1b3d76");
+  root.style.setProperty("--couleur4", "#152f5b");
   clair = true;
   console.log(clair);
   btnmode.innerHTML = '<i class="bx bxs-moon"></i>';
   localStorage.setItem("sombreclair", "clair");
 }
+
+// Logo
+const logo = document.querySelector(".logo");
+const h1 = document.querySelector(".logo h1");
+const h3 = document.querySelector(".logo h3");
+
+logo.addEventListener("mouseenter", function () {
+  h1.style.color = cs.getPropertyValue("--couleur4");
+  h3.style.color = cs.getPropertyValue("--couleur4");
+  h3.style.borderBlock = `${cs.getPropertyValue("--couleur4")} solid 2px`;
+});
+
+logo.addEventListener("mouseleave", function () {
+  h1.style.color = cs.getPropertyValue("--couleur3");
+  h3.style.color = cs.getPropertyValue("--couleur3");
+  h3.style.borderBlock = `${cs.getPropertyValue("--couleur3")} solid 2px`;
+});
+
+// Categories
+
+const cat1 = document.querySelector(".cat1");
+const cat2 = document.querySelector(".cat2");
+const cat3 = document.querySelector(".cat3");
+
+cat1.addEventListener("mouseenter", function () {
+  cat1.style.backgroundImage = "url(../images/categorie-romansado.png)";
+});
+cat1.addEventListener("mouseleave", function () {
+  cat1.style.backgroundImage = "url(../images/categorie-romansadov2.png)";
+});
+
+cat2.addEventListener("mouseenter", function () {
+  cat2.style.backgroundImage = "url(../images/categorie-romansado.png)";
+});
+cat2.addEventListener("mouseleave", function () {
+  cat2.style.backgroundImage = "url(../images/categorie-romansadov2.png)";
+});
+
+cat3.addEventListener("mouseenter", function () {
+  cat3.style.backgroundImage = "url(../images/categorie-romansado.png)";
+});
+cat3.addEventListener("mouseleave", function () {
+  cat3.style.backgroundImage = "url(../images/categorie-romansadov2.png)";
+});
